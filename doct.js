a9212a8dea077a64324ebdef9a5f0477fc0e109e
sarhak234 <168266094+sarhak234@users.noreply.github.com>
2024-05-08 20:35:51 +0530

    Update doct.js

--- doct.js
+++ doct.js
@@ -26,11 +26,9 @@ async function fetchurl() {
                 // Splitting the definition into an array of points
                 if (data.definition == '') {
                     def.innerHTML='defenation not found';
-                } else {
-                    let definitionPoints = data.definition.split('\n');
-
+                } else{
                     // Joining the points with line breaks
-                    def.innerHTML = definitionPoints.join('<br>');
+                    def.innerHTML = data.definition.replace('1.','<br>1.').replace('2.','<br>2.').replace('3.','<br>3.').replace('4.','<br>4.').replace('5.','<br>5.').replace('6.','<br>6.').replace('7.','<br>7.').replace('8.','<br>8.').replace('9.','<br>9.').replace('10.','<br>10.');
                 }
 
             } catch (error) {
