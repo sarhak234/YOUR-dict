244fa2cafcdf3210ddc7887e7893d298efb8e11b
sarhak234 <168266094+sarhak234@users.noreply.github.com>
2024-05-09 12:37:05 +0530

    Update doct.js

--- doct.js
+++ doct.js
@@ -28,7 +28,8 @@ async function fetchurl() {
                     def.innerHTML='defenation not found';
                 } else{
                     // Joining the points with line breaks
-                    def.innerHTML = data.definition.replace('1.','<br>1.').replace('2.','<br>2.').replace('3.','<br>3.').replace('4.','<br>4.').replace('5.','<br>5.').replace('6.','<br>6.').replace('7.','<br>7.').replace('8.','<br>8.').replace('9.','<br>9.').replace('10.','<br>10.');
+                    def.innerHTML = data.definition.replace('1.','<br>1.').replace('2.','<br>2.').replace('3.','<br>3.').replace('4.','<br>4.').replace('5.','<br>5.').replace('6.','<br>6.').replace('7.','<br>7.').replace('8.','<br>8.').replace('9.','<br>9.').replace('10.','<br>10.').replace(/;/g, ',<br>');
+
                 }
 
             } catch (error) {
